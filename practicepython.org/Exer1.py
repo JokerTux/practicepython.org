9cdbd41c587f71216b927b7a1981545fb34b6b36
Patrick <49305052+JokerTux@users.noreply.github.com>
2019-04-06 17:22:39 +0200

    Update Exer1.py

--- practicepython.org/Exer1.py
+++ practicepython.org/Exer1.py
@@ -2,7 +2,7 @@ import time
 
 
 name = input ("Enter your name : ")
-age  = int (input ("Please enter your age (0-200) :"))
+age  = int (input ("Please enter your age (0-99) :"))
 till = 100 - age
 
 print ("Welcome ",name,"your ",age," years old . In ",till," you will be 100 years old." )
