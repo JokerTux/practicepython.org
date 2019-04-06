3bd450a9d964106a7abb11cf7f4707cf0057daf6
Patrick <49305052+JokerTux@users.noreply.github.com>
2019-04-06 11:52:46 +0200

    Update Exer3e.py

--- practicepython.org/Exer3e.py
+++ practicepython.org/Exer3e.py
@@ -4,7 +4,7 @@ a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 lessthe = []
 for items in a :
 	if items < 5:
-		lessthe.append(items) #Not sure if this works fine .
+		lessthe.append(items) 
 		print(lessthe)
 
 time.sleep(5)		
